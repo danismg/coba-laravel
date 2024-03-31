7a4ec79b83285175138fd77a3cd692de19e55f03
Daniel Andres Simangunsong <danit.balai@gmail.com>
2024-03-31 14:31:20 +0700

    dine

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-  <h1>Halaman Home</h1>
+<section class="hero-area">
+    <div class="container">
+        <!-- Konten Anda -->
+    </div>
+</section>
+@endsection
+
+@section('head')
+<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,26 +1,48 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <div class="container">
-      <a class="navbar-brand" href="/">WPU Blog</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
+<nav class="navbar navbar-expand-lg navbar-light bg-danger" style="height: 100px">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
       <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <div class="dropdown">
+                    <button class="nav-link {{ ($active === "profile") ? 'active' : '' }} dropdown-toggle" style="background: none; border: none; color: white; font-size: 1rem;" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Profile
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Visi Misi</a></li>
+                        <li><a class="dropdown-item" href="#">Sejarah Sekolah</a></li>
+                        <li><a class="dropdown-item" href="#">Prestasi</a></li>
+                    </ul>
+                </div>
+            </li>
               <li class="nav-item">
-                  <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" href="/">Home</a>
+                  <a class="nav-link {{ ($active === "tenaga kerja ") ? 'active' : '' }}" href="/posts">tenaga Kerja</a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="/about">About</a>
-              </li>
+                <div class="dropdown">
+                    <button class="nav-link {{ ($active === "galery") ? 'active' : '' }} dropdown-toggle" style="background: none; border: none; color: white; font-size: 1rem;" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        galery
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Fasilitas sekolah</a></li>
+                        <li><a class="dropdown-item" href="#">kegiatan sekolah</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ ($active === "pengumuman") ? 'active' : '' }}" href="/pengumuman">pengumuman</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ ($active === "saran") ? 'active' : '' }}" href="/saran">saran</a>
+            </li>
               <li class="nav-item">
-                  <a class="nav-link {{ ($active === "posts") ? 'active' : '' }}" href="/posts">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
+                <a class="nav-link {{ ($active === "contact") ? 'active' : '' }}" href="/contact">contact</a>
             </li>
           </ul>
-
           <ul class="navbar-nav ms-auto">
                 @auth
                   <li class="nav-item dropdown">
@@ -44,7 +66,7 @@
                 </li>
                 @endauth
             </ul>
-          
+
       </div>
   </div>
 </nav>
